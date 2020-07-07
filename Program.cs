959ccb8facc7108848cb47dbbf4a85590ef52459
Dmitry Svalov <me@agentd.ru>
2020-07-07 12:37:08 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using CalcCS.Expressions;
+using CalcCS.Parser;
+using System;
+
+namespace CalcCS
+{
+    /// <summary>
+    /// Основная программа
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter the mathematical expression containing floating point numbers, operations +, -, *, / or ():\n> ");
+            string expr_str = Console.ReadLine();
+
+            Parser.Parser parser = new Parser.Parser(expr_str);
+
+            try
+            {
+                IExpression expr = parser.Parse();
+                Console.WriteLine(expr.Calculate());
+            }
+            catch (ParserException pex)
+            {
+                Console.WriteLine("Parser error: " + pex.Message);
+            }
+            catch (DivideByZeroException dze)
+            {
+                Console.WriteLine("Calculation error: " + dze.Message);
+            }
+            catch (InvalidOperationException ioe)
+            {
+                Console.WriteLine("Programmer error: " + ioe.Message);
+            }
+            catch
+            {
+                Console.WriteLine("Unknown error");
+            }
+
+            Console.Write("Press <Enter> key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
